93055eb8e086de9b9c32ee69e69c17fc2a83c4ed
guohaomeng <3020721699@qq.com>
2022-06-05 12:29:15 +0800

    优化起摆速度

--- main.m
+++ main.m
@@ -6,17 +6,17 @@ out=sim('./R_InvertedPendulum.slx');
 
 %% 绘图1
 figure(1);
-plot(out.t,out.bv,'-',out.t,out.bp,'--b',out.t,out.mv,':b','LineWidth',2);
-axis([0 5 -80 80]);
+plot(out.t,out.energy*100,'-',out.t,out.bp*10,'--b',out.t,out.mv,':b','LineWidth',2);
+axis([0 0.8 -30 40]);
 xlabel('t(时间/s）','fontsize',12);
 ylabel('y(输出)','fontsize',12);
 title('旋转倒立摆变量监控','fontsize',16);
-legend('摆杆角速度', '摆杆位置','电机旋转速度');
+legend('摆杆能量(放大100倍后)', '摆杆位置(放大10倍后)','电机旋转速度');
 
 %% 绘图2
 figure(2);
 plot(out.t,out.bp,'-','LineWidth',2);
-axis([0 5 -3.2 3.2]);
+axis([0 1 -3.2 3.2]);
 xlabel('t(时间/s）','fontsize',12);
 ylabel('摆杆位置[rad]','fontsize',12);
 title('旋转倒立摆摆杆位置变化图','fontsize',16);
@@ -36,3 +36,10 @@ axis([0 5 -25 25]);
 xlabel('t(时间/s）','fontsize',12);
 ylabel('电机旋转速度[rad/s]','fontsize',12);
 title('旋转倒立摆电机旋转速度变化图','fontsize',16);
+%% 绘图5
+figure(5);
+plot(out.t,out.energy,'-','LineWidth',2);
+axis([0 1 -0.1 0.2]);
+xlabel('t(时间/s）','fontsize',12);
+ylabel('倒立摆摆杆能量','fontsize',12);
+title('旋转倒立摆摆杆能量变化图','fontsize',16);
